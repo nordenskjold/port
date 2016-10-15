51024fc40e4e6b6cbc599a22e883c9a310d50f37
nordenskjold <ellen.nordenskjold@gmail.com>
2016-10-15 21:56:28 +0200

    Delete connection.php

--- admin/includes/connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysql_connect("ellenn.se.mysql", "ellenn_se", "znsQab3h") or die("Could not connect to the database");
-$db_selected = mysql_select_db("ellenn_se", $con) or die("Could not select correct database");
-?>
